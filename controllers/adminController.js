06be9fc87f3cd0185e93a566a4e58143ee54b6d3
PrincyChauhan <chauhanprincee7@gmail.com>
2025-01-07 15:07:22 +0530

    Admin login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res) => {
   try {
@@ -26,4 +27,31 @@ const signup = async (req, res) => {
   }
 };
 
-export { signup };
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Admin not found." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    if (user.role !== "admin") {
+      return res.status(403).json({ message: "Only admins can sign in." });
+    }
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ message: "Signin successfully.", token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error during signin." });
+  }
+};
+
+export { signup, signin };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/adminController.js";
+import { signup, signin } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/signup", signup);
+adminRouter.post("/signin", signin);
 
 export default adminRouter;
