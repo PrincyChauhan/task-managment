dfd223e8ae913fdf93726a6ca1049d6c65059ce2
PrincyChauhan <chauhanprincee7@gmail.com>
2025-01-11 18:46:56 +0530

    Change in sidebar

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Sidebar from "./SideBar";
-import { Link } from "react-router-dom";
 import UserListing from "./UserListing";
 
 const Dashboard = () => {
@@ -11,14 +10,6 @@ const Dashboard = () => {
         <h1 className="text-3xl font-bold">Welcome to the dashboard</h1>
         <br />
         <UserListing />
-        <div className="mt-6">
-          <Link
-            to="/dashboard/tasks/create"
-            className="bg-blue-500 text-white px-4 py-2 rounded"
-          >
-            Create Task
-          </Link>
-        </div>
       </div>
     </div>
   );
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import TaskCreation from "./TaskCreation";
-
-// const Sidebar = () => {
-//   return (
-//     <div className="flex">
-//       <div className="w-64 h-screen bg-gray-800 text-white">
-//         <div className="p-6 text-xl font-semibold text-center">
-//           Admin Dashboard
-//         </div>
-//         <ul className="space-y-4 p-6">
-//           {/* User Section Link */}
-//           <li>
-//             <Link to="/dashboard/users" className="hover:text-gray-400">
-//               <div className="text-lg">Users</div>
-//             </Link>
-//           </li>
-
-//           {/* Task Section Link */}
-//           <li>
-//             <Link to="/dashboard/tasks" className="hover:text-gray-400">
-//               <div className="text-lg">Tasks</div>
-//             </Link>
-//           </li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Sidebar;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -45,14 +12,14 @@ const Sidebar = () => {
           {/* User Section Link */}
           <li>
             <Link to="/admin/user" className="hover:text-gray-400">
-              <div className="text-lg">Users</div>
+              <div className="text-lg">Create Users</div>
             </Link>
           </li>
 
           {/* Task Section Link */}
           <li>
             <Link to="/dashboard/tasks" className="hover:text-gray-400">
-              <div className="text-lg">Tasks</div>
+              <div className="text-lg">Create Tasks</div>
             </Link>
           </li>
         </ul>
